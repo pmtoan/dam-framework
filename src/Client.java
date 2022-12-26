e82c8c397d169e043f94c50f2ba0518486390975
Toàn <contact.pmtoan@gmail.com>
2022-12-26 23:35:41 +0700

    Client
    Update how to use pattern in Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,16 @@
-import java.lang.reflect.Field;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.Date;
 
+import ConnectAdapter.ConnectionAdapter;
+import ConnectAdapter.MySQLConnection;
+import Expression.Composite.AndExpression;
+import Expression.Composite.OrExpression;
+import Expression.IExpression;
+import Expression.Leaf.*;
 import Statement.StatementAdapter;
+import Statement.StatementBuilder;
+import Statement.MySQLStatementBuilder;
 import Statement.MySQLStatement;
 
 /**
@@ -16,55 +21,66 @@ import Statement.MySQLStatement;
  */
 public class Client {
     public static void main(String args[]){
+        // Oracle:      jdbc:oracle:thin:[<user>/<password>]@<host>[:<port>]:<SID>
+        // MySQL:       jdbc:mysql://mysql.db.server:3306/my_database?useSSL=false&serverTimezone=UTC
+        // SQLServer:   jdbc:sqlserver://mssql.db.server\\mssql_instance;databaseName=my_database
+        // PostgreSQL:  jdbc:postgresql://host:port/database?properties
+
         String dbURL = "jdbc:mysql://localhost:3306/sakila";
+
+        String host = "localhost";
+        String port = "3306";
+        String databaseName = "sakila";
         String userName = "root";
         String password = "";
 
-        Actor c = new Actor("Chris", "Devil Gamer");
+        Actor actor = new Actor("Cris", "Devil Gamer");
 
-        for(Field a :  c.getClass().getFields()){
-            try {
-                System.out.println(a.getName() + ": " + a.get(c));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
+        ConnectionAdapter connection = new MySQLConnection(host, port, databaseName, userName, password);
+        System.out.println(connection.getDbUrl());
+        connection.connect();
+
+        StatementAdapter myStatement = connection.createStatement();
+
+        StatementBuilder statementBuilder = myStatement.createStatementBuilder();
+
+        String query = statementBuilder.table("actor")
+                //.select(new String[] {"first_name", "last_name"})
+                .selectAll()
+                //.where(new Equal("first_name", "Cris"))
+                //.groupBy("first_name")
+                .having(new GreaterOrEqual("actor_id", 210))
+                .createQueryStatement();
+
+        String create = statementBuilder
+                .table("actor")
+                .insert(actor)
+                .createUpdateStatement();
+
+        String update = statementBuilder
+                .table("actor")
+                .update(actor)
+                .where(new Equal("actor_id", 239))
+                .createUpdateStatement();
+
+        String delete = statementBuilder
+                .table("actor")
+                .delete()
+                .where(new AndExpression(new Equal("first_name", "cris"),
+                        new Equal("last_name", "devil gamer")))
+                .createUpdateStatement();
+
+        //System.out.println("Row affected: " + myStatement.executeRawSQLUpdate(delete));
+
+        System.out.println(query);
+        ResultSet rs = myStatement.executeRawSQLQuery(query);
+
+        List<Map<String, Object>> result = new ArrayList<>();
 
 
-        Connection conn = null;
-        try {
-            //approach 1
-            //Class.forName("com.mysql.cj.jdbc.Driver");
-
-            //approach 2
-            Driver myDriver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(myDriver);
-
-            // Create connection
-            conn = DriverManager.getConnection(dbURL, userName, password);
-            System.out.println("connect successfully!");
-
-            StatementAdapter stmt = new MySQLStatement(conn.createStatement());
-
-            //stmt.table("actor").insert(c).executeUpdate();
-
-            //stmt.table("actor").update(c).where("actor_id='235'").executeUpdate();
-
-            stmt.table("actor").delete().where("first_name='chris' and last_name='hemsworth'").executeUpdate();
-
-            ResultSet rs = stmt.table("actor").selectAll().where("first_name='chris'").executeQuery();
-            //System.out.println(s.executeUpdate());
-
-            //int rs1 = stmt.table("actor").insert(new QueryStatement(conn.createStatement())).executeUpdate();
-
-
-//            ResultSet rs = stmt.table("actor")
-//                    .select(new String[] {"first_name", "last_name"}).selectAll()
-//                    //.where("first_name = 'Chris'")
-//                    .executeQuery();
-            List<Map<String, Object>> result = new ArrayList<>();
+        try{
             while (rs.next()) {
-                Map<String, Object> resMap = new HashMap<>();
+                Map<String, Object> resMap = new LinkedHashMap<>();
                 for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
                     resMap.put(rs.getMetaData().getColumnName(i), rs.getObject(i));
                 }
@@ -81,11 +97,11 @@ public class Client {
                 }
                 System.out.println();
             }
-            // close connection
-            conn.close();
-        } catch (Exception ex) {
-            System.out.println("connect failure!");
-            ex.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
+
+
+        connection.close();
     }
 }
